3d6638fbf179154257bc5f093367be3c1264be28
RoshanKumar770 <roshankumar81624@gmail.com>
2025-01-16 15:38:20 +0530

    basic updated

--- basics.py
+++ basics.py
@@ -1,22 +1,23 @@
+# Enter and printing (Concatitation)
+
 num = int(input("Enter an integer: "))
 print("You entered:", num)
-
 num1 = int(input("Enter another integer: "))
 print("You entered:", num1)
 
-sum_result = num + num1
+sum_result = num + num1  # Addition 
 print("Addition of two numbers is:", sum_result)
 
-sub_result = num - num1
+sub_result = num - num1 # Subtration
 print("Subtraction of two numbers is:", sub_result)
 
-if num1 != 0:
+if num1 != 0:   # Division
     div_result = num / num1
     print("Division of two numbers is:", div_result)
 else:
     print("Division by zero is not allowed.")
 
-if num1 != 0:
+if num1 != 0:   # Modulus
     rem_result = num % num1
     print("Remainder is:", rem_result)
 else:
@@ -28,17 +29,25 @@ if num1 != 0:
 else:
     print("Cannot perform integer division by zero.")
 
+    # Power Function
+
 pow_result = num ** num1
 print("Power result is:", pow_result)
+
+   # Implicit Function
 
 a = 10
 b = 10.5
 c = a + b
 print(c)
 
+   # Explicit Function
+
 x = 3.14
 y = int(x)
 print(y)
+ 
+ # Relational Operators
 
 a = 21
 b = 13
@@ -49,12 +58,17 @@ print(p)
 
 P = (9 == 9)
 Q = (7 > 5)
+ 
+ # Logical Operator
+
 R = P and Q
 S = P or Q
 T = not P
 print(R)
 print(S)
 print(T)
+ 
+ # Bitwise Operator
 
 a = 12
 x = a >> 2
@@ -66,5 +80,12 @@ b = 12
 c = a + (b >> 1)
 print(c)
 
+ # Printing function
+
 name = 'Roshan'
 print(name)
+
+ # Eval function
+exp = "3+4"
+result=eval(exp)
+print(result)
